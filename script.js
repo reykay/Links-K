dc5340e5f2a1376b6d72a43853c23ac746ce8281
Vieira <123202113+reykay@users.noreply.github.com>
2026-04-25 12:51:18 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 function copiarPix() {
   navigator.clipboard.writeText("46991416144");
-  alert("Pix copiado: 46991416144");
+
+  const toast = document.getElementById("toast");
+  toast.classList.add("show");
+
+  setTimeout(() => {
+    toast.classList.remove("show");
+  }, 3000);
 }
